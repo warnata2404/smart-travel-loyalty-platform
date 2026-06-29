27e58f158e8e941c626ca453c4a05f50174cab45
warnata2404 <411231103@mahasiswa.undira.ac.id>
2026-06-29 23:31:04 +0700

    feat(model): complete route model

--- app/Models/Route.php
+++ app/Models/Route.php
@@ -2,9 +2,52 @@
 
 namespace App\Models;
 
+use App\Enums\RouteStatus;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Route Model
+ *
+ * @property int $id
+ * @property int $destination_id
+ * @property string $origin_name
+ * @property string $destination_name
+ * @property float $distance_km
+ * @property int $estimated_duration_minutes
+ * @property float $base_price
+ * @property RouteStatus $route_status
+ */
 class Route extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'destination_id',
+        'origin_name',
+        'destination_name',
+        'distance_km',
+        'estimated_duration_minutes',
+        'base_price',
+        'route_status',
+    ];
+
+    /**
+     * Attribute casting.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'distance_km' => 'decimal:2',
+            'base_price' => 'decimal:2',
+            'route_status' => RouteStatus::class,
+        ];
+    }
 }
