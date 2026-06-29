4517d67f1ae8fe627c34433b28c8f2568e3a9261
warnata2404 <411231103@mahasiswa.undira.ac.id>
2026-06-30 00:29:22 +0700

    feat(controller): create destination controller foundation

---
+++ app/Http/Controllers/DestinationController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Destination\StoreDestinationRequest;
+use App\Http\Requests\Destination\UpdateDestinationRequest;
+use App\Models\Destination;
+use App\Services\Destination\DestinationService;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+
+class DestinationController extends Controller
+{
+    public function __construct(
+        private readonly DestinationService $destinationService,
+    ) {}
+
+    public function index(): View {}
+
+    public function create(): View {}
+
+    public function store(
+        StoreDestinationRequest $request,
+    ): RedirectResponse {}
+
+    public function edit(
+        Destination $destination,
+    ): View {}
+
+    public function update(
+        UpdateDestinationRequest $request,
+        Destination $destination,
+    ): RedirectResponse {}
+
+    public function destroy(
+        Destination $destination,
+    ): RedirectResponse {}
+}
