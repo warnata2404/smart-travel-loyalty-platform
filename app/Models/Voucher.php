106913b887374ef9e5c92caf5a3eaf4e475a36ca
warnata2404 <411231103@mahasiswa.undira.ac.id>
2026-06-29 23:41:49 +0700

    feat(model): complete voucher model

--- app/Models/Voucher.php
+++ app/Models/Voucher.php
@@ -2,9 +2,57 @@
 
 namespace App\Models;
 
+use App\Enums\VoucherStatus;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Voucher Model
+ *
+ * Represents a loyalty reward voucher.
+ */
 class Voucher extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'voucher_code',
+
+        'user_id',
+
+        'loyalty_rule_id',
+
+        'used_booking_id',
+
+        'milestone_km',
+
+        'discount_percentage',
+
+        'voucher_status',
+
+        'used_at',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'milestone_km' => 'decimal:2',
+
+            'discount_percentage' => 'decimal:2',
+
+            'voucher_status' => VoucherStatus::class,
+
+            'used_at' => 'datetime',
+        ];
+    }
 }
