a433365693919a8a68f3b088ab1a25fcd360a7b3
warnata2404 <411231103@mahasiswa.undira.ac.id>
2026-06-29 23:28:58 +0700

    feat(model): complete destination model

--- app/Models/Destination.php
+++ app/Models/Destination.php
@@ -2,9 +2,42 @@
 
 namespace App\Models;
 
+use App\Enums\DestinationStatus;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Destination Model
+ *
+ * @property int $id
+ * @property string $name
+ * @property string|null $description
+ * @property DestinationStatus $destination_status
+ */
 class Destination extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'destination_status',
+    ];
+
+    /**
+     * Attribute casting.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'destination_status' => DestinationStatus::class,
+        ];
+    }
 }
